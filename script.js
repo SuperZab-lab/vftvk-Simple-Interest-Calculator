c6c70bd36e9340a7c383d337fb537dd5bc422786
SuperZab-lab <93475630+SuperZab-lab@users.noreply.github.com>
2021-11-12 18:09:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,25 @@ var year=new Date().getFullYear()+parseInt(years);
 function updateRate()
 {var rateval= (document.getElementById("rate").value;
 document.getElementById("rate_val").innertext=rateval;}
-
+document.getElementById("result").innerHTML = "<br />If you deposit <mark>"+principal+"</mark>,<br /> at an interest rate of <mark>"+rate+"%</mark>.<br />You will receive an amount of <mark>"+interest+"</mark><br />in the year <mark>"+finalYear+"</mark>";
+//update value
+function getSliderValue() 
+function showrate(x)
+// Function updates span with final value from slider
+{
+    document.getElementById("enteredRate").innerText = x;
+}
+function checkdata()
+// Function returns false if Amount entered is less than or equal to zero
+{
+    var principal = document.getElementById("principal");
+    if (principal.value<=0){
+        alert("Enter a positive number");
+        principal.focus();
+        return false;
+    }
+    return true;
+}
     
 
         
